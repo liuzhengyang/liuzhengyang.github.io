ebb7ba9187c4fede74cf3106c9fecc821b72f574
liuzhengyang <liuzhengyang@users.noreply.github.com>
2024-10-18 03:25:22 +0000

    Auto deploy from Github Actions

---
+++ assets/js/cg-10.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{439:function(n,t,e){"use strict";e.r(t);var u={name:"NotFound",mounted:function(){this.$router.push("/")}},o=e(34),s=Object(o.a)(u,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=s.exports}}]);
